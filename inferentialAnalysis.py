4b7dad9870363b3677824fe58931e94b3b352804
tala3849 <taylor.a.lawrence@colorado.edu>
2018-12-02 23:59:51 -0700

    Submit

--- inferentialAnalysis.py
+++ inferentialAnalysis.py
@@ -56,7 +56,7 @@ runAnova(rawData, moreComplex)
 
 
 ########################
-#MONDAY WORK 
+#MONDAY WORK 11.12
 ########################
 
 #Question 1 
@@ -73,7 +73,7 @@ runAnova(rawData, moreComplex)
     
     #dependent -  time 
     
-    #stat test - generialize regression when you have have 
+    #stat test - generialize regression when deal with something overtime 
     
 #C) Over the last 10 years, have there been more hikers on average in Estes Park in the spring or summer? 
     # independent- season cat.
@@ -90,5 +90,16 @@ runAnova(rawData, moreComplex)
     # stat test - chi squared 
     
 #Question 2 
+    
+    
+################################
+#MONDAY WORK 11.26 
+################################
+
+#Question 3 - the data quality issue is that within the csv there is data for the males that has a space after Male and within the code it did not have the space so it was only grabbing the male data that did not have a space and that was only 3 of the 7. To fix this we went into the csv and changed it to not having a space so it grabbed all 7. 
+#does the department correlate with admission the results stayed the same but the T test which is the statistic and P value with the alter data. 
+
+#Question 4 - in Jupyter notebook 
+    
     
 
